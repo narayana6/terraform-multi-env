0967c0f06ffb5857eaadf365d6eb465b2a778b7f
unknown <user.name>
2026-04-07 13:54:30 +0530

    multi-env

---
+++ tfvars/dev/backend.tf
@@ -0,0 +1,4 @@
+bucket = "bnsaws-dev"
+key    = "multi-env-tfvars"
+region = "us-east-1"
+dynamodb_table = "bnsaws-dev"
---
+++ tfvars/prod/backend.tf
@@ -0,0 +1,4 @@
+bucket = "bnsaws-prod"
+key    = "multi-env-tfvars"
+region = "us-east-1"
+dynamodb_table = "bnsaws-prod"
