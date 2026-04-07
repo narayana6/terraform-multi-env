c1584e01c94926f4204fd6b17461e18afe7e87d9
unknown <user.name>
2026-04-07 15:19:37 +0530

    multi

--- tfvars/ec2.tf
+++ tfvars/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "expense" {
     for_each = var.instance_names # each.key and each.value
     ami = data.aws_ami.ami_info.id
-    vpc_security_group_ids = ["sg-0fea5e49e962e81c9"] # replace with you SG ID
+    vpc_security_group_ids = ["sg-03fabbef88a55c9b0"] # replace with you SG ID
     instance_type = each.value
     tags = merge(
         var.common_tags,
